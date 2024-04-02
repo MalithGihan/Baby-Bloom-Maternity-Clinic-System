c53d5f9e532b191ff1f33718247637094102bdaf
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-02 16:06:47 +0530

    Commenced the coding part of mama login page

---
+++ assets/pages/mama-login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Baby Bloom - Mama Login</title>
+        <link rel="icon" type="image/x-icon" href="assets/images/logos/bb-favicon.png">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <script rel="script" type="text/js" href="assets/js/bootstrap.min.js"></script>
+        <style>
+            
+        </style>
+    </head>
+<body>
+
+</body>
+</html>
