82d8edee818ff3de1277dc8a59ba9a5c64555883
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-02 15:15:10 +0530

    Added font files and completed the main interface

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Baby Bloom - Materity Clinic System</title>
+        <link rel="icon" type="image/x-icon" href="assets/images/logos/bb-favicon.png">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <script rel="script" type="text/js" href="assets/js/bootstrap.min.js"></script>
+        <style>
+            :root{
+                --bg: #EFEBEA;
+                --light-txt: #0D4B53;
+                --light-txt2:#000000;
+                --dark-txt: #86B6BB;
+            }
+        </style>
+    </head>
+    <body class=d-flex flex-row>
+        <div class="index-txt-field">
+            <img src="assets/images/logos/babybloom-main-logo.webp" alt="BabyBloom main logo">
+            <h1 class="index-title">Baby Bloom</h1>
+            <h3 class="index-subtitle"> Maternity Clinic System</h3>
+        </div>
+    </body>
+</html>
