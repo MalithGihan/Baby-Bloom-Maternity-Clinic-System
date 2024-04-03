bc6ce067783d2102c24cb42d53a0f3103c1089ce
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-04 01:16:01 +0530

    Supplement order page completed with styling

--- assets/pages/mama-order-supplement.php
+++ assets/pages/mama-order-supplement.php
@@ -12,7 +12,56 @@
         <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
         <script rel="script" type="text/js" href="../js/bootstrap.min.js"></script>
         <style>
-            
+            :root{
+                --bg: #EFEBEA;
+                --light-txt: #0D4B53;
+                --light-txt2:#000000;
+                --dark-txt: #86B6BB;
+            }
+            @font-face {
+                font-family: 'Inter-Bold'; /* Heading font */
+                src: url('../font/Inter-Bold.ttf') format('truetype'); 
+                font-weight: 700;
+            }
+            @font-face {
+                font-family: 'Inter-Light'; /* Text font */
+                src: url('../font/Inter-Light.ttf') format('truetype'); 
+                font-weight: 300;
+            }
+
+            body{
+                margin:0 !important;
+                padding:0 !important;
+                background-color: var(--bg) !important;
+            }
+            .quota-form{
+                gap:1rem;
+            }
+            .quota-container{
+                font-family: 'Inter-Bold';
+            }
+            .quota-sub-container{
+                font-size:2rem;
+            }
+            .deliver-method{
+                font-family: 'Inter-Light';
+                flex-direction:row;
+                gap:1rem;
+            }
+            .quota-form-btn{
+                background-color:var(--light-txt);
+                color:var(--bg);
+                font-family: 'Inter-Bold';
+                border:0px;
+                border-radius:10rem;
+                padding:0.8rem 2rem;
+                transition: 0.6s;
+            }
+            .quota-form-btn:hover{
+                background-color:var(--dark-txt);
+                color:var(--bg);
+                transition: 0.6s;
+            }
         </style>
     </head>
 <body>
@@ -50,17 +99,15 @@
                         <p class="quota-static-value"> / 1</p>
                     </div>
                 </div>
-                <div class="quota-form">
-                    <form method="POST">
-                        <div class="deliver-method d-flex">
-                            <input type="radio" id="home-deliver" name="delivery-method" value="Home Delivery">
-                            <label for="home-deliver">Home Delivery</label><br>
-                            <input type="radio" id="pickup" name="delivery-method" value="Pickup">
-                            <label for="pickup">Pickup</label>
-                        </div>
-                        <input type="submit" value="Order">
-                    </form>
-                </div>
+                <form method="POST" class="quota-form d-flex flex-column align-items-center">
+                    <div class="deliver-method d-flex">
+                        <input type="radio" id="home-deliver" name="delivery-method" value="Home Delivery">
+                        <label for="home-deliver">Home Delivery</label><br>
+                        <input type="radio" id="pickup" name="delivery-method" value="Pickup">
+                        <label for="pickup">Pickup</label>
+                    </div>
+                    <input type="submit" value="Order" class="quota-form-btn">
+                </form>
             </div>
             <div class="main-footer d-flex flex-row justify-content-start">
                 <a href="#">
