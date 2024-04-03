6b07fddd49783948748ea8e97a070471652766e9
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-03 15:27:18 +0530

    Mama dashboard UI completed for all devices

---
+++ assets/pages/mama-dashboard.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Baby Bloom - Mama Dashboard</title>
+        <link rel="icon" type="image/x-icon" href="../images/logos/bb-favicon.png">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <script rel="script" type="text/js" href="../js/bootstrap.min.js"></script>
+        <style>
+            
+        </style>
+    </head>
+<body>
+    <div class="common-container d-flex">
+        <header class="d-flex flex-row justify-content-between align-items-center">
+            <img src="../images/logos/bb-top-logo.webp" alt="BabyBloom top logo" class="common-header-logo">
+            <div class="d-flex flex-column">
+                <h1 class="common-title">BabyBloom</h1>
+                <h3 class="common-description">Maternity Clinic System</h3>
+            </div>
+        </header>
+        <main>
+            <div class="main-header d-flex">
+                <h2 class="main-header-title">DASHBOARD</h2>
+                <div class="main-usr-data d-flex flex-column">
+                    <div class="usr-data-container d-flex">
+                        <img src="../images/mama-image.png" alt="User profile image" class="usr-image">
+                        <div class="usr-data d-flex flex-column">
+                            <div class="username">Jane Doe</div>
+                            <div class="useremail">janedoe@gmail.com</div>
+                        </div>
+                    </div>
+                    <div class="usr-logout-btn">
+                        <a href="##">
+                            <button class="usr-lo-btn">Log out</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content d-flex">
+                <a href="#" class="option">
+                    <div class="d-flex flex-column align-items-center">
+                        <img src="../images/mama-dashbaord/option1.png" class="option-img">
+                        <p class="option-name">Boook Appointment</p>
+                    </div>   
+                </a>
+                <a href="#" class="option">
+                    <div class="d-flex flex-column align-items-center">
+                        <img src="../images/mama-dashbaord/option2.png" class="option-img">
+                        <p class="option-name">Order Supplement</p>
+                    </div>   
+                </a>
+                <a href="#" class="option">
+                    <div class="d-flex flex-column align-items-center">
+                        <img src="../images/mama-dashbaord/option3.png" class="option-img">
+                        <p class="option-name">View QR Code</p>
+                    </div>   
+                </a>
+            </div>
+        </main>
+    </div>
+
+    <script>
+    </script>
+</body>
+</html>
--- assets/pages/staff-login.php
+++ assets/pages/staff-login.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Baby Bloom - Mama Login</title>
+        <title>Baby Bloom - Staff Login</title>
         <link rel="icon" type="image/x-icon" href="../images/logos/bb-favicon.png">
         <link rel="stylesheet" type="text/css" href="../css/style.css">
         <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
