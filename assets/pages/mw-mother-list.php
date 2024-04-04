d8c7b75a624f878a7eac1df455e06cd783384ec0
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-04 21:36:06 +0530

    Added mother health report form and styling is done

---
+++ assets/pages/mw-mother-list.php
@@ -0,0 +1,179 @@
+<?php
+include 'dbaccess.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Baby Bloom - REGISTERED MOTHERS</title>
+        <link rel="icon" type="image/x-icon" href="../images/logos/bb-favicon.png">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <script rel="script" type="text/js" href="../js/bootstrap.min.js"></script>
+        <style>
+            :root{
+                --bg: #EFEBEA;
+                --light-txt: #0D4B53;
+                --light-txt2:#000000;
+                --dark-txt: #86B6BB;
+            }
+            @font-face {
+                font-family: 'Inter-Bold'; /* Heading font */
+                src: url('../font/Inter-Bold.ttf') format('truetype'); 
+                font-weight: 700;
+            }
+            @font-face {
+                font-family: 'Inter-Light'; /* Text font */
+                src: url('../font/Inter-Light.ttf') format('truetype'); 
+                font-weight: 300;
+            }
+
+            body{
+                margin:0 !important;
+                padding:0 !important;
+                background-color: var(--bg) !important;
+            }
+            .report-row{
+                justify-content: space-between;
+            }
+            .scan-qr-btn,#mom-search-btn{
+                font-family: 'Inter-Bold';
+                font-size:0.8rem;
+                background-color:var(--light-txt);
+                color:var(--bg);
+                border:0px;
+                border-radius:10rem;
+                padding:0.5rem 2rem;
+                transition:0.6s;
+            }
+            .scan-qr-btn:hover,#mom-search-btn:hover{
+                background-color:var(--dark-txt);
+                transition:0.6s;
+            }
+            .mom-search-continer{
+                gap:1rem;
+            }
+            #mom-nic-search{
+                font-family: 'Inter-Bold';
+                font-size:0.8rem;
+                color:var(--light-txt);
+                outline:none;
+                background-color:var(--bg);
+                border:2px solid var(--light-txt);
+                border-radius:10rem;
+                width:30vw;
+                text-align: center;
+            }
+            .mom-list-btn{
+                background-color:var(--dark-txt);
+                color:var(--bg);
+                font-family: 'Inter-Bold';
+                border:0px;
+                border-radius:10rem;
+                padding:0.5rem 2rem;
+                text-decoration: none;
+                transition:0.6s;
+            }
+            .mom-list-btn:hover{
+                background-color:var(--light-txt);
+                color:var(--bg);
+                transition:0.6s;
+            }
+            th,td{
+                background-color:var(--bg) !important;
+            }
+            td{
+                color:var(--light-txt);
+                font-family: 'Inter-Light';
+            }
+            .table-btn-container{
+                gap:1rem;
+            }
+
+        </style>
+    </head>
+<body>
+    <div class="common-container d-flex">
+        <header class="d-flex flex-row justify-content-between align-items-center">
+            <img src="../images/logos/bb-top-logo.webp" alt="BabyBloom top logo" class="common-header-logo">
+            <div class="d-flex flex-column">
+                <h1 class="common-title">BabyBloom</h1>
+                <h3 class="common-description">Maternity Clinic System</h3>
+            </div>
+        </header>
+        <main>
+            <div class="main-header d-flex">
+                <h2 class="main-header-title">REGISTERED MOTHERS</h2>
+                <div class="main-usr-data d-flex flex-column">
+                    <div class="usr-data-container d-flex">
+                        <img src="../images/mama-image.png" alt="User profile image" class="usr-image">
+                        <div class="usr-data d-flex flex-column">
+                            <div class="username">Jane Doe</div>
+                            <div class="useremail">janedoe@gmail.com</div>
+                        </div>
+                    </div>
+                    <div class="usr-logout-btn">
+                        <a href="##">
+                            <button class="usr-lo-btn">Log out</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content d-flex flex-column">
+                <div class="report-row d-flex">
+                    <button class="scan-qr-btn" id="scan-qr-btn">Scan QR</button>
+                    <form class="mom-search-continer d-flex" method="POST">
+                        <input type="text" id="mom-nic-search" name="vaccine-name" placeholder="Enter Mother NIC" required>
+                        <input type="submit" name="submit" value="Search" id="mom-search-btn">
+                    </form>
+                </div>
+                <table class="table">
+                    <?php
+                        
+                        $sql = "SELECT * FROM pregnant_mother";
+                            $result = mysqli_query($con,$sql);
+                            if($result){
+                                $num = mysqli_num_rows($result);
+                                echo "$num results found.";
+                                if($num > 0){
+                                    while($row = mysqli_fetch_assoc($result)){
+                                        echo '
+                                        <tbody>
+                                            <tr class="vaccine-results">
+                                                <td>'.$row['firstName'].'</td>
+                                                <td>'.$row['surname'].' </td>
+                                                <td>'.$row['NIC'].'</td>
+                                                <td class="table-btn-container d-flex flex-row justify-content-center">
+                                                    <a class="mom-list-btn" href="mw-health-details.php?id='.$row["NIC"].'">Health report</a>
+                                                    <a class="mom-list-btn" href="mw-vaccination-details.php?id='.$row["NIC"].'">Vaccination report</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>';
+                                    }
+                                }
+                                else{
+                                    echo '<h3>Data not found</h3>';
+                                }
+                            }
+/*
+                        if(isset($_POST['submit'])){
+                            $search = $_POST['vaccine-name'];
+
+                            
+                        } */
+                    ?>
+                </table>
+            </div>
+            <div class="main-footer d-flex flex-row justify-content-start">
+                <a href="../pages/midwife-dashboard.php">
+                    <button class="main-footer-btn">Return</button>
+                </a>
+            </div>
+        </main>
+    </div>
+
+    <script>
+    </script>
+</body>
+</html>
