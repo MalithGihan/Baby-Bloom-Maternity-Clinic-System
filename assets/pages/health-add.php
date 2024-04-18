5975069cd20ce0f8d8418e39438d5c258b20cfc5
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-04-18 21:50:05 +0530

    Added session start and session details checking code

--- assets/pages/health-add.php
+++ assets/pages/health-add.php
@@ -1,20 +1,26 @@
 <?php
+session_start();
 include 'dbaccess.php';
 
-  $NIC = $_POST['mama-nic']; 
-  $date = $_POST['hr-date'];
-  $appx_Next_date = $_POST['hr-appx-date'];
-  $heartRate = $_POST['hr-heart-rate'];
-  $bloodPressure = $_POST['hr-blood-pss']; 
-  $cholesterolLevel = $_POST['hr-chol-lvl'];
-  $heartRateConclution = $_POST['hr-heart-conclusion']; 
-  $bloodPressureConclution = $_POST['hr-blood-pss-conclusion'];
-  $weight = $_POST['hr-weight'];
-  $babyMovement = $_POST['hr-baby-movement'];
-  $babyHeartbeat = $_POST['hr-baby-heart-rate']; 
-  $scanConclution = $_POST['hr-scan-conclusion'];
-  $vcabnormalitiescBy = $_POST['hr-abnormalities'];
-  $sprcial_Instruction = $_POST['hr-spec-instructions'];
+if (!isset($_SESSION["staffEmail"])) {
+    header("Location: staff-login.php"); // Redirect to pregnant mother login page
+    exit();
+}
+
+$NIC = $_POST['mama-nic']; 
+$date = $_POST['hr-date'];
+$appx_Next_date = $_POST['hr-appx-date'];
+$heartRate = $_POST['hr-heart-rate'];
+$bloodPressure = $_POST['hr-blood-pss']; 
+$cholesterolLevel = $_POST['hr-chol-lvl'];
+$heartRateConclution = $_POST['hr-heart-conclusion']; 
+$bloodPressureConclution = $_POST['hr-blood-pss-conclusion'];
+$weight = $_POST['hr-weight'];
+$babyMovement = $_POST['hr-baby-movement'];
+$babyHeartbeat = $_POST['hr-baby-heart-rate']; 
+$scanConclution = $_POST['hr-scan-conclusion'];
+$vcabnormalitiescBy = $_POST['hr-abnormalities'];
+$sprcial_Instruction = $_POST['hr-spec-instructions'];
   
 
 $sql = "INSERT INTO health_report (date, heartRate, bloodPressure, cholesterolLevel, weight, heartRateConclusion, bloodPressureConclusion, babyMovement, babyHeartbeat, scanConclusion, abnormalities, special_Instruction, appx_Next_date, NIC) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
