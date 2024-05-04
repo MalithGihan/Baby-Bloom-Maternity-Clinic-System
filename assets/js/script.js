9dd31aceb28333b2ba70ebab8143d642f18a5a6c
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-05-04 23:26:27 +0530

    Added profile page url when clicking the usr-image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
-console.log("gg");
-alert("gg");
+console.log("Script loaded!");
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with class "usr-image"
+    const usrImages = document.querySelectorAll('.usr-image');
+
+    // Loop through each element and add event listener
+    usrImages.forEach(function(usrImage) {
+        usrImage.addEventListener('click', function() {
+            window.location.href="staff-profile.php";
+        });
+    });
+});
