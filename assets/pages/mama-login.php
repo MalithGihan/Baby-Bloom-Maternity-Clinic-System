93d0e2f0579c7e12556bc91cebdcb92fd975099f
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-05-23 16:01:03 +0530

    Added mother redirection condition if already logged in

--- assets/pages/mama-login.php
+++ assets/pages/mama-login.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if(isset($_SESSION["mamaEmail"])){
+    header("Location: mama-dashboard.php");
+    exit();
+}
+
 include 'dbaccess.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
